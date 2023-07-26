eaf5e5ab4b15b4bc8c594a54ae44e773db875cc0
jesuspaz3000 <veyito30000@gmail.com>
2023-07-26 17:42:17 -0500

    creacion de carpeta para los componentes

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Main from './src/components/Main';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Esto es una prueba</Text>
-      <StatusBar style="auto" />
+      <Main />
     </View>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from 'react-native';
+
+const Main = () => {
+    return (
+        <View>
+            <Text>Probando esta vaina xD</Text>
+        </View>
+    )
+}
+
+export default Main
